e473536f6da39fb7848528d1ec436c97100ecb84
Bob131 <bentleytyde10@gmail.com>
2026-06-20 07:55:30 -0400

    Add image upload event listener to update image source

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const upload = document.getElementById("imageUpload")
+const upload = document.getElementById("imageUpload");
 const image = document.getElementById("discImage");
+upload.addEventListener(
+  "change",
+  function(){
+  image.src = URL.createObjectURL(upload.files[0]);  
+  }
+);
