c2c300629d98d6f598eb6c5b6719c48bd2e54474
Bob131 <bentleytyde10@gmail.com>
2026-07-08 22:45:48 -0400

    Refactor dragging logic in script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let dragging = false;
-
 const upload = document.getElementById("imageUpload");
 const image = document.getElementById("discImage");
 const workspace = document.getElementById("workspace");
 const centerDot = document.getElementById("centerDot");
+
+let dragging = false;
 
 upload.addEventListener("change", function(){
   
@@ -23,11 +23,31 @@ centerDot.addEventListener("mousedown",function(){
 
 document.addEventListener("mousemove",function(event){
 
+  const rect = workspace.getBoundingClientRect();
+  const mouseX = event.clientX - rect.left;
+  const mouseY = event.clientY - rect.top;
+
   if (dragging) {
-    const rect= workspace.getBoundingClientRect();
+
     centerDot.style.left = (event.clientX - rect.left - 10) + "px";
     centerDot.style.top = (event.clientY - rect.top - 10) + "px";
+    
   }
+
+  if (rotating) {
+
+    const centerX = rect.left + 400;
+    const centerY = rect.top + 400;
+    
+    const dx = event.clientX - centerX;
+    const dy = event.clientY - centerY;
+    
+    const angle = Math.atan2(dx, dy);
+
+    startGuide.style.transform = `rotate(${angle}rad)`;
+    
+  }
+  
 });
 
 document.addEventListener("mouseup",function(){
